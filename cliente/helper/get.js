9101c37db85786c8e0483d34aa2d67f7a0a2b7ee
Alvaro <salazares2005@gmail.com>
2026-05-13 16:12:44 -0500

    FIX: Reparacion de los puertos

--- cliente/helper/get.js
+++ cliente/helper/get.js
@@ -4,7 +4,7 @@
  * @returns {promise<any>} - los datos devueltos por la base de datos en formato json.
  */
 export const get = async (url) => {
-    const solicitud = await fetch(`http://localhost:3000/${url}`);
+    const solicitud = await fetch(`http://10.5.225.158:3000/${url}`);
     const data = await solicitud.json()
     return data;
 }
