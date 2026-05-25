5bdc87dc61868ec4da29b3ad8a6bf0ceaff4da74
Alvaro <salazares2005@gmail.com>
2026-05-25 12:57:04 -0500

    Fix: se han arreglado la escritura de unos comentarios

--- cliente/src/router/enrrutador.js
+++ cliente/src/router/enrrutador.js
@@ -1,10 +1,10 @@
 import { rutas } from "./rutas.js";
 
 export const enrrutador = async (app) => {
-  // 1. ruta por defecto si el usuario acaba de entrar a la raiz
+  // rruta por defecto si el usuario acaba de entrar a la raiz
   let hash = window.location.hash || '#/productos';
 
-  // 2. buscamos la ruta en nuestro arreglo
+  // buscamos la ruta en nuestro arreglo
   let rutaActual = rutas.find((ruta) => ruta.ruta === hash);
   // renderizamos la vista y ejecutamos el controlador
   app.innerHTML = rutaActual.vista();
